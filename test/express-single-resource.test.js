6dbfd6f626766023bffda87b922e09e10789e0ec
Lynn <code@krogseng.com>
2017-02-08 10:22:15 -0800

    Establishing baseline files and directories. Passing npm test.

---
+++ test/express-single-resource.test.js
@@ -0,0 +1,8 @@
+// just getting npm test to pass
+assert = require('assert');
+describe('nothing to see here yet', () => {
+    it('this is going to work', done => {
+        assert.equal(1, 1);
+        done();
+    });
+});
