8b5d0bcbb48a2449540775b658c699b081e69540
Clarefollett <chlaw101@gmail.com>
2017-02-08 11:35:13 -0800

    two passing and they can both fail because they were before I fixed spelling errors

--- test/schnoodles.test.js
+++ test/schnoodles.test.js
@@ -19,6 +19,31 @@ describe('schnoodles REST HTTP API', () => {
         return request.get('/schnoodles')
             .then(request => request.body)
             .then(schnoodles => assert.deepEqual(schnoodles, []));
-    })
+    });
 
-})
+    const breezy = {
+        name: 'breezy',
+        type: 'naughty dog'
+    };
+
+    let princess = {
+        name: 'princess',
+        type: 'naughty dog'
+    };
+
+    function saveSchnoodle(schnoodle) {
+        return request.post('/schnoodles')
+            .send(schnoodle)
+            .then(response => response.body);
+    }
+
+    it('saves a schnoodle', () => {
+        return saveSchnoodle(breezy)
+            .then(savedSchnoodle => {
+                assert.isOk(savedSchnoodle._id);
+                breezy._id = savedSchnoodle._id;
+                assert.deepEqual(savedSchnoodle, breezy);
+            });
+    });
+
+});
