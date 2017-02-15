d9dbef97173c80a932812b5bf13bd8578d1a7800
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-15 09:38:03 -0800

    final for submittal

--- test/trees.test.js
+++ test/trees.test.js
@@ -3,7 +3,7 @@ const assert = chai.assert;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-process.env.MONGODB_URI= 'mongodb://localhost:27017/trees-test'
+process.env.MONGODB_URI= 'mongodb://localhost:27017/trees-test';
 const connection = require('../lib/connection');
 const mongoose = require('mongoose');
 
@@ -48,8 +48,9 @@ describe('trees REST HTTP API', () => {
     it('saves a tree', () => {
         return saveTree(englishOak)
             .then(savedTree => {
-                assert.isOk(savedTree._id);;
-                englishOak._id = savedTree._id;;
+                assert.isOk(savedTree._id);
+                englishOak._id = savedTree._id;
+                englishOak.__v = 0;
                 assert.deepEqual(savedTree, englishOak);
             });
     });
@@ -61,7 +62,7 @@ describe('trees REST HTTP API', () => {
             });
     });
 
-    it.skip('gets a list of trees', () => {
+    it('gets a list of trees', () => {
         return Promise.all([
             saveTree(ponderosaPine),
             saveTree(bigLeafMaple)
@@ -73,36 +74,47 @@ describe('trees REST HTTP API', () => {
         .then(() => request.get('/trees'))
         .then(res => {
             const trees = res.body;
-            assert.deepEqual(trees, [englishOak, ponderosaPine, bigLeafMaple])
+            assert.deepEqual(trees, [englishOak, ponderosaPine, bigLeafMaple]);
         });
     });
 
-    it.skip('updates saved tree', () => {
+    it('updates saved tree', () => {
         englishOak.genus = 'Ulmus';
         return request.put(`/trees/${englishOak._id}`)
             .send(englishOak)
             .then(res => {
                 assert.deepEqual(res.body, englishOak);
-                return request.get(`/trees/${englishOak._id}`)
+                return request.get(`/trees/${englishOak._id}`);
             })
             .then(res => {
-                assert.deepEqual(res.body.genus, englishOak.genus)
+                assert.deepEqual(res.body.genus, englishOak.genus);
             });
     });
 
-    it.skip('deletes a tree', () => {
+    it('deletes a tree', () => {
+        return request.del(`/trees/${ponderosaPine._id}`)
+            .then(res => {
+                assert.isTrue(res.body.deleted);
+            });
+    });
+
+    it('returns false if data to delete does not exist', () => {
         return request.delete(`/trees/${ponderosaPine._id}`)
             .then(res => {
-                assert.isTrue(res.body.deleted)
+                assert.isFalse(res.body.deleted);
             });
     });
 
-    it.skip('returns false if data to delete does not exist', () => {
-        return request.delete(`/trees/${ponderosaPine._id}`)
-            .then(res => {
-                assert.isFalse(res.body.deleted)
-            });
+    it('return error 404 on non-existing id get', () => {
+        return request.get('/trees/589df44aedd27e7cad8f7d0a')
+            .then(
+                () => { throw new Error('successful status code not expected');},
+                res => {
+                    assert.equal(res.status, 404);
+                    assert.ok(res.response.body.error);
+                } //not throwing error 
+            );
     });
 
-    after(() => connection.close())
+    after(() => connection.close());
 });
